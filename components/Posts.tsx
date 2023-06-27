28063f88ee77de26de850532ffd05d0dfab71924
kseniya <kseniyamelnnik@gmail.com>
2023-06-27 12:24:50 +0300

    feat: add types to post

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import {IPost} from "@/types";
 
 type PostsProps = {
     posts: any[]
 }
 export const Posts = ({posts}: PostsProps) => {
     return <ul className='w-[90%] max-w-[1200px] flex flex-col gap-2.5 text-lg'>
-        {posts.map((post: any)=> (
+        {posts.map((post: IPost)=> (
             <li key={post.id} className='hover:text-amber-700'>
                 <Link href={`/blog/${post.id}`}>{post.title}</Link>
             </li>
